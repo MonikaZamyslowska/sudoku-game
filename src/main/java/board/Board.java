907c5cc2085174dfad096541ce1e76b06662c2c5
MonikaZamyslowska <zamyslowska.monika@gmail.com>
2018-09-20 13:24:04 +0200

    Improvement methods and adding solve algorithm

--- src/main/java/board/Board.java
+++ src/main/java/board/Board.java
@@ -1,23 +1,42 @@
 package board;
 
 import lombok.Getter;
+import resolve.SudokuSolver;
 
 import static service.Statements.*;
 
 @Getter
 public class Board {
+    SudokuSolver sudokuSolver = new SudokuSolver();
     private int[][] board;
+    private int[][] copyBoard;
 
     public Board(int[][] board) {
         this.board = board;
     }
 
-    public void printBoard() {
+    public void printBoard(int[][] board) {
         for (int row = BOARD_START_INDEX; row < MAX_VALUE; row++) {
             for (int col = BOARD_START_INDEX; col < MAX_VALUE; col++) {
                 System.out.print(board[row][col] + " | ");
             }
             System.out.println();
         }
+    }
+
+    public int[][] solveSudoku(int[][] board) {
+        copyBoard = deepCopy(board);
+        sudokuSolver.solve(copyBoard);
+        return copyBoard;
+    }
+
+    public int[][] deepCopy(int[][] board) {
+        int[][] copyBoard = new int[BOARD_SIZE][BOARD_SIZE];
+        for (int r = BOARD_START_INDEX; r < BOARD_SIZE; r++) {
+            for (int c = BOARD_START_INDEX; c < BOARD_SIZE; c++) {
+                copyBoard[r][c] = board[r][c];
+            }
+        }
+        return copyBoard;
     }
 }
--- src/main/java/resolve/SudokuSolver.java
+++ src/main/java/resolve/SudokuSolver.java
@@ -5,12 +5,30 @@ import static service.Statements.*;
 public class SudokuSolver {
 
     public boolean solve(int[][] board) {
-        return false;
+
+        for (int row = BOARD_START_INDEX; row < BOARD_SIZE; row++) {
+            for (int col = BOARD_START_INDEX; col < BOARD_SIZE; col++) {
+                if (board[row][col] == EMPTY) {
+                    for (int num = MIN_VALUE; num <= MAX_VALUE; num++) {
+                        if (isValid(board, row, col, num)) {
+                            board[row][col] = num;
+                            if (solve(board)) {
+                                return true;
+                            } else {
+                                board[row][col] = EMPTY;
+                            }
+                        }
+                    }
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 
     public boolean isValid(int[][] board, int row, int col, int num) {
-        return !containsInRow(board, row, num) && !containsInColumn(board, col, num) &&
-                !containsInSubsection(board, row, col, num);
+        return !(containsInRow(board, row, num) || containsInColumn(board, col, num) ||
+                containsInSubsection(board, row, col, num));
     }
 
     public boolean containsInRow(int[][] board, int row, int num) {
@@ -32,21 +50,19 @@ public class SudokuSolver {
     }
 
     public boolean containsInSubsection(int[][] board, int row, int col, int num) {
-        int contRowStart = row - row / SUBSECTION_SIZE;
+        int contRowStart = (row / SUBSECTION_SIZE) * 3;
         int contRowEnd = contRowStart + SUBSECTION_SIZE;
 
-        int contColStart = col - col / SUBSECTION_SIZE;
+        int contColStart = (col / SUBSECTION_SIZE) * 3;
         int contColEnd = contColStart + SUBSECTION_SIZE;
 
         for (int r = contRowStart; r < contRowEnd; r++) {
             for (int c = contColStart; c < contColEnd; c++) {
-                if (board[r][c]  == num) {
+                if (board[r][c] == num) {
                     return true;
                 }
             }
         }
         return false;
     }
-
-
 }
