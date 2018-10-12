b1d9df2bc2e8f2f135e9c09492d4e12dc80a647b
MonikaZamyslowska <zamyslowska.monika@gmail.com>
2018-10-12 13:44:51 +0200

    Update

--- src/main/java/service/SudokuService.java
+++ src/main/java/service/SudokuService.java
@@ -21,7 +21,7 @@ public class SudokuService {
     public static String WELCOME_MESSAGE = "WELCOME TO SUDOKU :)";
     public static String ASK_LEVEL = "Choose Level: \n EASY -> 1 \n MEDIUM -> 2 \n HARD -> 3";
     public static String SET_VALUE = "Enter filed to set and value (column row value) or enter \"SUDOKU\" " +
-            "if you wont to solve Sudoku. ";
+            "if you want to solve Sudoku. ";
     public static String PLAY_AGAIN = "Do You want to play again? \n If yes -> \"y\" \n If no -> anything ;)";
     public static String FINISH_GAME = "Do you want to finish game? \n If yes -> \"y\" \n If no -> anything ;)";
     public static Pattern PATTERN_VALUE = Pattern.compile("^([1-9],[1-9],[1-9],?)+?$");
